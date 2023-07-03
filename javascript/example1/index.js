fc171e5726211dd710c41024e74ccead9331c7a0
Shivachauhan17 <shivanbd2019@gmail.com>
2023-07-03 23:45:43 +0530

    first javascript style

---
+++ javascript/example1/index.js
@@ -0,0 +1,8 @@
+document.getElementById("green").onclick=Green
+document.getElementById("purple").onclick=Purple
+document.getElementById("yellow").onclick=Yellow
+
+function Green(){
+    document.querySelector('body').style.background='rgba(0, 255, 0,0.4)'
+    document.querySelector('body').style.color='white'
+}
