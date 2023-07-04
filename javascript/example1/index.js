ac0cd97638b2b1fba70c829424317acc395c0a09
Shivachauhan17 <shivanbd2019@gmail.com>
2023-07-04 21:37:35 +0530

    some more javascript

--- javascript/example1/index.js
+++ javascript/example1/index.js
@@ -6,3 +6,13 @@ function Green(){
     document.querySelector('body').style.background='rgba(0, 255, 0,0.4)'
     document.querySelector('body').style.color='white'
 }
+
+function Purple(){
+    document.querySelector('body').style.background='rgba(128,0,128,0.6)'
+    document.querySelector('body').style.color='white'
+}
+
+function Yellow(){
+    document.querySelector('body').style.background='rgba(255,255,0,1)'
+    document.querySelector('body').style.color='white'
+}
