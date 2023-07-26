560029d7f49d27234a13b83df661b28c75f14444
Shivachauhan17 <shivanbd2019@gmail.com>
2023-07-26 23:31:33 +0530

    wrote some client side js

---
+++ simple_crud_app/public/js/main.js
@@ -0,0 +1,31 @@
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+rray.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteRapper)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+async function deleteRapper(){
+    const sName = this.parentNode.childNodes[1].innerText
+    const bName = this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteRapper', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'stageNameS': sName,
+              'birthNameS': bName
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
